bab87e14bfc82053b038864cd2a9f1ab5b48c6f5
MURILLO FERREIRA MARTINS <murillo_f_martins@estudante.sesisenai.org.br>
2026-07-20 21:29:39 -0300

    #

--- src/controller/study-plan.controller.js
+++ src/controller/study-plan.controller.js
@@ -1,26 +1,25 @@
-import { GoogleGenAI, Type } from "@google/genai"
+import { GoogleGenAI } from "@google/genai"
+import { quizSchema, responseSchema } from "../gemini/schemas.gemini"
 
 export {
     createTeachingPlan,
 }
 
-import { quizSchema, responseSchema } from "../gemini/schemas.gemini"
+const ai = new GoogleGenAI({ apiKey: process.env.API_KEY })
 
 async function createTeachingPlan(req, res) {
     try {
-        const { theme, note } = req.body
-
-        const ai = new GoogleGenAI({ apiKey: process.env.API_KEY })
+        const { theme, note, difficulty } = req.body
 
         const prompt = `
             Você é um avaliador pedagógico profissional.
             Crie um plano de estudos progressivo para o assunto: "${theme}".
             Observação adicional do usuário: "${note}".
+            Dificuldade desejada pelo usuário: "${difficulty}".
             Além disso, gere uma Avaliação Diagnóstica com 5 questões de múltipla escolha.
             Este teste servirá para medir o conhecimento prévio do aluno antes de iniciar os estudos,
             cobrindo conceitos base do tema.
         `
-
         const response = await ai.models.generateContent({
             model: "gemini-2.5-flash",
             contents: prompt,
